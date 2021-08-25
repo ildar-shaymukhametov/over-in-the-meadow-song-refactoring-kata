10e5edf8bb661b3eb66b6e3cbcb9a0c3d18411a0
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-25 23:04:41 +0500

    add song

--- js/src/program.js
+++ js/src/program.js
@@ -1,3 +1,93 @@
 export class Program {
-  
+  song() {
+    return "Over in the meadow\n" +
+      "In the sand, in the sun\n" +
+      "Lived an old mother toadi\n" +
+      "And her little toadie one\n" +
+      "\"Jump!\" said the mother\n" +
+      "\"I jump!\" said the one\n" +
+      "So they jumped and they jumped\n" +
+      "In the sand in the sun\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "Where the stream runs blue\n" +
+      "Lived an old mother fis\n" +
+      "And her little fishes two\n" +
+      "\"Swim!\" said the mother\n" +
+      "\"We swim!\" said the two\n" +
+      "So they swam and they swam\n" +
+      "Where the stream runs blue\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "In a hole in a tree\n" +
+      "Lived an old mother bluebir\n" +
+      "And her little birdies three\n" +
+      "\"Sing!\" said the mother\n" +
+      "\"We sing!\" said the three\n" +
+      "So they sang and they sang\n" +
+      "In their home in a tree\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "By the reeds on the shore\n" +
+      "Lived an old mother muskra\n" +
+      "And her little ratties four\n" +
+      "\"Dive!\" said the mother\n" +
+      "\"We dive!\" said the four\n" +
+      "So they dived and they splashed\n" +
+      "By the reeds on the shore\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "In a snug beehive\n" +
+      "Lived a mother honey be\n" +
+      "And her little honies five\n" +
+      "\"Buzz!\" said the mother\n" +
+      "\"We buzz!\" said the five\n" +
+      "So they buzzed and they buzzed\n" +
+      "In their snug beehive\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "In a nest made of sticks\n" +
+      "Lived a black mother cro\n" +
+      "And her little crows six\n" +
+      "\"Caw!\" said the mother\n" +
+      "\"We caw!\" said the six\n" +
+      "So they cawed and they cawed\n" +
+      "In their nest made of sticks\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "In the grass soft and even\n" +
+      "Lived a mother cricke\n" +
+      "And her little crickets seven\n" +
+      "\"Chirp!\" said the mother\n" +
+      "\"We chirp!\" said the seven\n" +
+      "So they chirped cheery notes\n" +
+      "In the grass soft and even\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "On an old mossy gate\n" +
+      "Lived a brown mother lizar\n" +
+      "And her little lizards eight\n" +
+      "\"Bask!\" said the mother\n" +
+      "\"We bask!\" said the eight\n" +
+      "So they basked, and they basked\n" +
+      "On the old mossy gate\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "Where the cool pools shine\n" +
+      "Lived a green mother fro\n" +
+      "And her little froggies nine\n" +
+      "\"Croak!\" said the mother\n" +
+      "\"We croak!\" said the nine\n" +
+      "So they croaked and they croaked\n" +
+      "Where the cool pools shine\n" +
+      "\n" +
+      "Over in the meadow\n" +
+      "In a sly little den\n" +
+      "Lived a gray mother spide\n" +
+      "And her little spiders ten\n" +
+      "\"Spin!\" said the mother\n" +
+      "\"We spin!\" said the ten\n" +
+      "So they spun silken webs\n" +
+      "In their sly little den\n"
+  }
 }
