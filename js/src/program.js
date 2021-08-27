9661f7bfa1aed5234c17a7ed3bcbef288b0eea4f
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-27 08:20:35 +0500

    rename function

--- js/src/program.js
+++ js/src/program.js
@@ -3,90 +3,90 @@ export class Program {
     return "Over in the meadow,\n" +
       `${location(0, true)},\n` +
       `Lived ${mother(0)}\n` +
-      `And her little ${children(0)} ${numberToWord(1)}.\n` +
+      `And her little ${children(0)} ${numberOfChildren(1)}.\n` +
       `\"${action(0)}!\" said the mother;\n` +
-      `\"${pronoun(1)} ${action(0).toLowerCase()}!\" said the ${numberToWord(1)}.\n` +
+      `\"${pronoun(1)} ${action(0).toLowerCase()}!\" said the ${numberOfChildren(1)}.\n` +
       `So they ${actionDone(0)},\n` +
       `${location(0)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(1, true)},\n` +
       `Lived ${mother(1)}\n` +
-      `And her little ${children(1)} ${numberToWord(2)}.\n` +
+      `And her little ${children(1)} ${numberOfChildren(2)}.\n` +
       `\"${action(1)}!\" said the mother;\n` +
-      `\"${pronoun(2)} ${action(1).toLowerCase()}!\" said the ${numberToWord(2)}.\n` +
+      `\"${pronoun(2)} ${action(1).toLowerCase()}!\" said the ${numberOfChildren(2)}.\n` +
       `So they ${actionDone(1)},\n` +
       `${location(1)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(2, true)},\n` +
       `Lived ${mother(2)}\n` +
-      `And her little ${children(2)} ${numberToWord(3)}.\n` +
+      `And her little ${children(2)} ${numberOfChildren(3)}.\n` +
       `\"${action(2)}!\" said the mother;\n` +
-      `\"${pronoun(3)} ${action(2).toLowerCase()}!\" said the ${numberToWord(3)}.\n` +
+      `\"${pronoun(3)} ${action(2).toLowerCase()}!\" said the ${numberOfChildren(3)}.\n` +
       `So they ${actionDone(2)},\n` +
       `${location(2)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(3, true)},\n` +
       `Lived ${mother(3)}\n` +
-      `And her little ${children(3)} ${numberToWord(4)}.\n` +
+      `And her little ${children(3)} ${numberOfChildren(4)}.\n` +
       `\"${action(3)}!\" said the mother;\n` +
-      `\"${pronoun(4)} ${action(3).toLowerCase()}!\" said the ${numberToWord(4)}.\n` +
+      `\"${pronoun(4)} ${action(3).toLowerCase()}!\" said the ${numberOfChildren(4)}.\n` +
       `So they ${actionDone(3)},\n` +
       `${location(3)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(4, true)},\n` +
       `Lived ${mother(4)}\n` +
-      `And her little ${children(4)} ${numberToWord(5)}.\n` +
+      `And her little ${children(4)} ${numberOfChildren(5)}.\n` +
       `\"${action(4)}!\" said the mother;\n` +
-      `\"${pronoun(5)} ${action(4).toLowerCase()}!\" said the ${numberToWord(5)}.\n` +
+      `\"${pronoun(5)} ${action(4).toLowerCase()}!\" said the ${numberOfChildren(5)}.\n` +
       `So they ${actionDone(4)},\n` +
       `${location(4)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(5, true)},\n` +
       `Lived ${mother(5)}\n` +
-      `And her little ${children(5)} ${numberToWord(6)}.\n` +
+      `And her little ${children(5)} ${numberOfChildren(6)}.\n` +
       `\"${action(5)}!\" said the mother;\n` +
-      `\"${pronoun(6)} ${action(5).toLowerCase()}!\" said the ${numberToWord(6)}.\n` +
+      `\"${pronoun(6)} ${action(5).toLowerCase()}!\" said the ${numberOfChildren(6)}.\n` +
       `So they ${actionDone(5)},\n` +
       `${location(5)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(6, true)},\n` +
       `Lived ${mother(6)}\n` +
-      `And her little ${children(6)} ${numberToWord(7)}.\n` +
+      `And her little ${children(6)} ${numberOfChildren(7)}.\n` +
       `\"${action(6)}!\" said the mother;\n` +
-      `\"${pronoun(7)} ${action(6).toLowerCase()}!\" said the ${numberToWord(7)}.\n` +
+      `\"${pronoun(7)} ${action(6).toLowerCase()}!\" said the ${numberOfChildren(7)}.\n` +
       `So they ${actionDone(6)},\n` +
       `${location(6)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(7, true)},\n` +
       `Lived ${mother(7)}\n` +
-      `And her little ${children(7)} ${numberToWord(8)}.\n` +
+      `And her little ${children(7)} ${numberOfChildren(8)}.\n` +
       `\"${action(7)}!\" said the mother;\n` +
-      `\"${pronoun(8)} ${action(7).toLowerCase()}!\" said the ${numberToWord(8)}.\n` +
+      `\"${pronoun(8)} ${action(7).toLowerCase()}!\" said the ${numberOfChildren(8)}.\n` +
       `So they ${actionDone(7)},\n` +
       `${location(7)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(8, true)},\n` +
       `Lived ${mother(8)}\n` +
-      `And her little ${children(8)} ${numberToWord(9)}.\n` +
+      `And her little ${children(8)} ${numberOfChildren(9)}.\n` +
       `\"${action(8)}!\" said the mother;\n` +
-      `\"${pronoun(9)} ${action(8).toLowerCase()}!\" said the ${numberToWord(9)}.\n` +
+      `\"${pronoun(9)} ${action(8).toLowerCase()}!\" said the ${numberOfChildren(9)}.\n` +
       `So they ${actionDone(8)},\n` +
       `${location(8)}.\n` +
       "\n" +
       "Over in the meadow,\n" +
       `${location(9, true)},\n` +
       `Lived ${mother(9)}\n` +
-      `And her little ${children(9)} ${numberToWord(10)}.\n` +
+      `And her little ${children(9)} ${numberOfChildren(10)}.\n` +
       `\"${action(9)}!\" said the mother;\n` +
-      `\"${pronoun(10)} ${action(9).toLowerCase()}!\" said the ${numberToWord(10)}.\n` +
+      `\"${pronoun(10)} ${action(9).toLowerCase()}!\" said the ${numberOfChildren(10)}.\n` +
       `So they ${actionDone(9)},\n` +
       `${location(9)}.`
   }
@@ -139,7 +139,7 @@ function children(number) {
   if (number == 9) return "spiders";
 }
 
-function numberToWord(number) {
+function numberOfChildren(number) {
   if (number == 1) return "one";
   if (number == 2) return "two";
   if (number == 3) return "three";
