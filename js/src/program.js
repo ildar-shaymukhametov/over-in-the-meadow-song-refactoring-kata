81f002dc8b62e6d8846468d9f9220fa3d1d8c0c1
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-27 07:32:08 +0500

    isolate children

--- js/src/program.js
+++ js/src/program.js
@@ -3,7 +3,7 @@ export class Program {
     return "Over in the meadow,\n" +
       `${location(0)},\n` +
       `Lived ${mother(0)}\n` +
-      "And her little toadie one.\n" +
+      `And her little ${children(0)} one.\n` +
       "\"Jump!\" said the mother;\n" +
       "\"I jump!\" said the one.\n" +
       "So they jumped and they jumped,\n" +
@@ -12,7 +12,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(1)},\n` +
       `Lived ${mother(1)}\n` +
-      "And her little fishes two.\n" +
+      `And her little ${children(1)} two.\n` +
       "\"Swim!\" said the mother;\n" +
       "\"We swim!\" said the two.\n" +
       "So they swam and they swam,\n" +
@@ -21,7 +21,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(2)},\n` +
       `Lived ${mother(2)}\n` +
-      "And her little birdies three.\n" +
+      `And her little ${children(2)} three.\n` +
       "\"Sing!\" said the mother;\n" +
       "\"We sing!\" said the three.\n" +
       "So they sang and they sang,\n" +
@@ -30,7 +30,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(3)},\n` +
       `Lived ${mother(3)}\n` +
-      "And her little ratties four.\n" +
+      `And her little ${children(3)} four.\n` +
       "\"Dive!\" said the mother;\n" +
       "\"We dive!\" said the four.\n" +
       "So they dived and they splashed,\n" +
@@ -39,7 +39,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(4)},\n` +
       `Lived ${mother(4)}\n` +
-      "And her little honies five.\n" +
+      `And her little ${children(4)} five.\n` +
       "\"Buzz!\" said the mother;\n" +
       "\"We buzz!\" said the five.\n" +
       "So they buzzed and they buzzed,\n" +
@@ -48,7 +48,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(5)},\n` +
       `Lived ${mother(5)}\n` +
-      "And her little crows six.\n" +
+      `And her little ${children(5)} six.\n` +
       "\"Caw!\" said the mother;\n" +
       "\"We caw!\" said the six.\n" +
       "So they cawed and they cawed,\n" +
@@ -57,7 +57,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(6)},\n` +
       `Lived ${mother(6)}\n` +
-      "And her little crickets seven.\n" +
+      `And her little ${children(6)} seven.\n` +
       "\"Chirp!\" said the mother;\n" +
       "\"We chirp!\" said the seven.\n" +
       "So they chirped cheery notes,\n" +
@@ -66,7 +66,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(7)},\n` +
       `Lived ${mother(7)}\n` +
-      "And her little lizards eight.\n" +
+      `And her little ${children(7)} eight.\n` +
       "\"Bask!\" said the mother;\n" +
       "\"We bask!\" said the eight.\n" +
       "So they basked, and they basked,\n" +
@@ -75,7 +75,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(8)},\n` +
       `Lived ${mother(8)}\n` +
-      "And her little froggies nine.\n" +
+      `And her little ${children(8)} nine.\n` +
       "\"Croak!\" said the mother;\n" +
       "\"We croak!\" said the nine.\n" +
       "So they croaked and they croaked,\n" +
@@ -84,7 +84,7 @@ export class Program {
       "Over in the meadow,\n" +
       `${location(9)},\n` +
       `Lived ${mother(9)}\n` +
-      "And her little spiders ten.\n" +
+      `And her little ${children(9)} ten.\n` +
       "\"Spin!\" said the mother;\n" +
       "\"We spin!\" said the ten.\n" +
       "So they spun silken webs,\n" +
@@ -116,4 +116,17 @@ function mother(number) {
   if (number == 7) return "a brown mother lizard";
   if (number == 8) return "a green mother frog";
   if (number == 9) return "a gray mother spider";
+}
+
+function children(number) {
+  if (number == 0) return "toadie";
+  if (number == 1) return "fishes";
+  if (number == 2) return "birdies";
+  if (number == 3) return "ratties";
+  if (number == 4) return "honies";
+  if (number == 5) return "crows";
+  if (number == 6) return "crickets";
+  if (number == 7) return "lizards";
+  if (number == 8) return "froggies";
+  if (number == 9) return "spiders";
 }
