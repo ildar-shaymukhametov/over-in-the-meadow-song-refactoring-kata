633fc69568a96246788b5a8836dc62b83b932d9c
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-28 22:54:47 +0500

    implement requirement

--- js/src/program.js
+++ js/src/program.js
@@ -1,16 +1,30 @@
 const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
 
 export class Program {
+  constructor() {
+    this.animals = [
+      new Animals("toadie", "toadie"),
+      new Animals("fish", "fish"),
+      new Animals("bluebird", "birdie"),
+      new Animals("muskrat", "rattie"),
+      new Animals("bee", "honie"),
+      new Animals("crow", "crow"),
+      new Animals("cricket", "cricket"),
+      new Animals("lizard", "lizard"),
+      new Animals("frog", "froggie"),
+      new Animals("spider", "spider"),
+    ];
+  }
   song() {
     var result = [];
     for (let i = 1; i <= 10; i++) {
-      result.push(this.verse(i, i));
+      result.push(this.verse({ verseNumber: i }));
     }
     return result.join("\n\n");
   }
-  verse(verseNumber, orderNumber) {
+  verse({ verseNumber, orderNumber = verseNumber, animalsNumber = verseNumber - 1 } = {}) {
     const childrenCount = this.childrenCountFor(orderNumber);
-    const verse = this.verseFor(verseNumber);
+    const verse = this.verseFor(verseNumber, animalsNumber);
 
     return "Over in the meadow,\n" +
       `${verse.firstLocation()},\n` +
@@ -31,7 +45,7 @@ export class Program {
 
     return new result(number);
   }
-  verseFor(number) {
+  verseFor(number, animalsNumber) {
     var result;
     try {
       result = eval(`Verse${number}`);
@@ -39,7 +53,7 @@ export class Program {
       result = Verse;
     }
 
-    return new result(number);
+    return new result(this.animals[animalsNumber]);
   }
 }
 
@@ -53,7 +67,23 @@ export class RandomSong extends Program {
     var nextNumber;
     var orderNumber = 1;
     while (nextNumber = this.randomizer.next()) {
-      result.push(this.verse(nextNumber, orderNumber++));
+      result.push(this.verse({ verseNumber: nextNumber, orderNumber: orderNumber++ }));
+    }
+    return result.join("\n\n");
+  }
+}
+
+export class RandomAnimalSong extends Program {
+  constructor(randomizer) {
+    super();
+    this.randomizer = randomizer;
+  }
+  song() {
+    var result = [];
+    var nextNumber;
+    var verseNumber = 1;
+    while ((nextNumber = this.randomizer.next()) !== null) {
+      result.push(this.verse({ verseNumber: verseNumber++, animalsNumber: nextNumber }));
     }
     return result.join("\n\n");
   }
@@ -73,11 +103,17 @@ class Article {
 }
 
 class Verse {
+  constructor(animals) {
+    this.animals = animals;
+  }
   firstLocation() {
     return this.location(true);
   }
   secondLocation() {
     return this.location();
+  }
+  child() {
+    return this.animals.child;
   }
 }
 
@@ -92,10 +128,7 @@ class Verse10 extends Verse {
     return `In ${new Article(isFirstAppearance)} sly little den`;
   }
   mother() {
-    return "a gray mother spider";
-  }
-  child() {
-    return "spider";
+    return `a gray mother ${this.animals.mother}`;
   }
 }
 
@@ -110,10 +143,7 @@ class Verse1 extends Verse {
     return "In the sand in the sun";
   }
   mother() {
-    return "an old mother toadie";
-  }
-  child() {
-    return "toadie";
+    return `an old mother ${this.animals.mother}`;
   }
 }
 
@@ -128,10 +158,7 @@ class Verse2 extends Verse {
     return "Where the stream runs blue";
   }
   mother() {
-    return "an old mother fish";
-  }
-  child() {
-    return "fish";
+    return `an old mother ${this.animals.mother}`;
   }
 }
 
@@ -146,10 +173,7 @@ class Verse3 extends Verse {
     return `In ${new Article(isFirstAppearance)} hole in a tree`;
   }
   mother() {
-    return "an old mother bluebird";
-  }
-  child() {
-    return "birdie";
+    return `an old mother ${this.animals.mother}`;
   }
 }
 class Verse4 extends Verse {
@@ -163,10 +187,7 @@ class Verse4 extends Verse {
     return "By the reeds on the shore";
   }
   mother() {
-    return "an old mother muskrat";
-  }
-  child() {
-    return "rattie";
+    return `an old mother ${this.animals.mother}`;
   }
 }
 class Verse5 extends Verse {
@@ -180,10 +201,7 @@ class Verse5 extends Verse {
     return `In ${new Article(isFirstAppearance)} snug beehive`;
   }
   mother() {
-    return "a mother honey bee";
-  }
-  child() {
-    return "honie";
+    return `a mother honey ${this.animals.mother}`;
   }
 }
 class Verse6 extends Verse {
@@ -197,10 +215,7 @@ class Verse6 extends Verse {
     return `In ${new Article(isFirstAppearance)} nest made of sticks`;
   }
   mother() {
-    return "a black mother crow";
-  }
-  child() {
-    return "crow";
+    return `a black mother ${this.animals.mother}`;
   }
 }
 class Verse7 extends Verse {
@@ -214,10 +229,7 @@ class Verse7 extends Verse {
     return "In the grass soft and even";
   }
   mother() {
-    return "a mother cricket";
-  }
-  child() {
-    return "cricket";
+    return `a mother ${this.animals.mother}`;
   }
 }
 class Verse8 extends Verse {
@@ -231,10 +243,7 @@ class Verse8 extends Verse {
     return "On the old mossy gate";
   }
   mother() {
-    return "a brown mother lizard";
-  }
-  child() {
-    return "lizard";
+    return `a brown mother ${this.animals.mother}`;
   }
 }
 class Verse9 extends Verse {
@@ -248,10 +257,7 @@ class Verse9 extends Verse {
     return "Where the cool pools shine";
   }
   mother() {
-    return "a green mother frog";
-  }
-  child() {
-    return "froggie";
+    return `a green mother ${this.animals.mother}`;
   }
 }
 class ChildrenCount {
@@ -317,5 +323,12 @@ class ChildrenCount1 extends ChildrenCount {
   }
   pluralize(child) {
     return child;
+  }
+}
+
+class Animals {
+  constructor(mother, child) {
+    this.mother = mother;
+    this.child = child;
   }
 }
