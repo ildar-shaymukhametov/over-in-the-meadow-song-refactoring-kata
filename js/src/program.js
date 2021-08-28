14675cdb5c120dff0c48836d29c152d64872cca8
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-28 14:36:47 +0500

    merge multiple conditionals into one

--- js/src/program.js
+++ js/src/program.js
@@ -34,36 +34,36 @@ class VerseNumber {
   }
   actionDone() {
     if (this.number == 2) return "swam and they swam";
-    if (this.number == 3) return "sang and they sang";
-    if (this.number == 4) return "dived and they splashed";
-    if (this.number == 5) return "buzzed and they buzzed";
-    if (this.number == 6) return "cawed and they cawed";
-    if (this.number == 7) return "chirped cheery notes";
-    if (this.number == 8) return "basked, and they basked";
-    if (this.number == 9) return "croaked and they croaked";
-    if (this.number == 10) return "spun silken webs";
+    else if (this.number == 3) return "sang and they sang";
+    else if (this.number == 4) return "dived and they splashed";
+    else if (this.number == 5) return "buzzed and they buzzed";
+    else if (this.number == 6) return "cawed and they cawed";
+    else if (this.number == 7) return "chirped cheery notes";
+    else if (this.number == 8) return "basked, and they basked";
+    else if (this.number == 9) return "croaked and they croaked";
+    else if (this.number == 10) return "spun silken webs";
   }
   action() {
     if (this.number == 2) return "swim";
-    if (this.number == 3) return "sing";
-    if (this.number == 4) return "dive";
-    if (this.number == 5) return "buzz";
-    if (this.number == 6) return "caw";
-    if (this.number == 7) return "chirp";
-    if (this.number == 8) return "bask";
-    if (this.number == 9) return "croak";
-    if (this.number == 10) return "spin";
+    else if (this.number == 3) return "sing";
+    else if (this.number == 4) return "dive";
+    else if (this.number == 5) return "buzz";
+    else if (this.number == 6) return "caw";
+    else if (this.number == 7) return "chirp";
+    else if (this.number == 8) return "bask";
+    else if (this.number == 9) return "croak";
+    else if (this.number == 10) return "spin";
   }
   location(isFirstAppearance) {
     if (this.number == 2) return "Where the stream runs blue";
-    if (this.number == 3) return `In ${article(isFirstAppearance)} hole in a tree`;
-    if (this.number == 4) return "By the reeds on the shore";
-    if (this.number == 5) return `In ${article(isFirstAppearance)} snug beehive`;
-    if (this.number == 6) return `In ${article(isFirstAppearance)} nest made of sticks`;
-    if (this.number == 7) return "In the grass soft and even";
-    if (this.number == 8) return "On the old mossy gate";
-    if (this.number == 9) return "Where the cool pools shine";
-    if (this.number == 10) return `In ${article(isFirstAppearance)} sly little den`;
+    else if (this.number == 3) return `In ${article(isFirstAppearance)} hole in a tree`;
+    else if (this.number == 4) return "By the reeds on the shore";
+    else if (this.number == 5) return `In ${article(isFirstAppearance)} snug beehive`;
+    else if (this.number == 6) return `In ${article(isFirstAppearance)} nest made of sticks`;
+    else if (this.number == 7) return "In the grass soft and even";
+    else if (this.number == 8) return "On the old mossy gate";
+    else if (this.number == 9) return "Where the cool pools shine";
+    else if (this.number == 10) return `In ${article(isFirstAppearance)} sly little den`;
 
     function article(isFirstAppearance) {
       if (isFirstAppearance) {
@@ -75,25 +75,25 @@ class VerseNumber {
   }
   mother() {
     if (this.number == 2) return "an old mother fish";
-    if (this.number == 3) return "an old mother bluebird";
-    if (this.number == 4) return "an old mother muskrat";
-    if (this.number == 5) return "a mother honey bee";
-    if (this.number == 6) return "a black mother crow";
-    if (this.number == 7) return "a mother cricket";
-    if (this.number == 8) return "a brown mother lizard";
-    if (this.number == 9) return "a green mother frog";
-    if (this.number == 10) return "a gray mother spider";
+    else if (this.number == 3) return "an old mother bluebird";
+    else if (this.number == 4) return "an old mother muskrat";
+    else if (this.number == 5) return "a mother honey bee";
+    else if (this.number == 6) return "a black mother crow";
+    else if (this.number == 7) return "a mother cricket";
+    else if (this.number == 8) return "a brown mother lizard";
+    else if (this.number == 9) return "a green mother frog";
+    else if (this.number == 10) return "a gray mother spider";
   }
   children() {
     if (this.number == 2) return "fishes";
-    if (this.number == 3) return "birdies";
-    if (this.number == 4) return "ratties";
-    if (this.number == 5) return "honies";
-    if (this.number == 6) return "crows";
-    if (this.number == 7) return "crickets";
-    if (this.number == 8) return "lizards";
-    if (this.number == 9) return "froggies";
-    if (this.number == 10) return "spiders";
+    else if (this.number == 3) return "birdies";
+    else if (this.number == 4) return "ratties";
+    else if (this.number == 5) return "honies";
+    else if (this.number == 6) return "crows";
+    else if (this.number == 7) return "crickets";
+    else if (this.number == 8) return "lizards";
+    else if (this.number == 9) return "froggies";
+    else if (this.number == 10) return "spiders";
   }
 }
 
@@ -121,15 +121,15 @@ class NumberOfChildren {
   }
   toString() {
     if (this.number == 1) return "one";
-    if (this.number == 2) return "two";
-    if (this.number == 3) return "three";
-    if (this.number == 4) return "four";
-    if (this.number == 5) return "five";
-    if (this.number == 6) return "six";
-    if (this.number == 7) return "seven";
-    if (this.number == 8) return "eight";
-    if (this.number == 9) return "nine";
-    if (this.number == 10) return "ten";
+    else if (this.number == 2) return "two";
+    else if (this.number == 3) return "three";
+    else if (this.number == 4) return "four";
+    else if (this.number == 5) return "five";
+    else if (this.number == 6) return "six";
+    else if (this.number == 7) return "seven";
+    else if (this.number == 8) return "eight";
+    else if (this.number == 9) return "nine";
+    else if (this.number == 10) return "ten";
   }
   pronoun() {
     if (this.number == 1) {
